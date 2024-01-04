d87ca4f4c075741012f2fff5a2fcbb3dfb3e71a0
oliveira00008 <gabrielgithub00008@gmail.com>
2024-01-04 14:31:00 -0300

    atu

--- html/js/script.js
+++ html/js/script.js
@@ -4,7 +4,7 @@ const switcher = document.querySelector('.btn');
 //esse metodo eo metodo toggle 
 switcher.addEventListener('click', function()
 {
-    document.body.classList.toggle('dark-theme')
+    document.body.classList.toggle('dark-theme');
 
     var className = document.body.className;
     if(className == "light-theme"){
